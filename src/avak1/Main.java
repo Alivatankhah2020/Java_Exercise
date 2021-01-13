6e6f5d8e7feb5b481ce7ca970bd45dd9800833f4
Alivatankhah2020 <74736616+Alivatankhah2020@users.noreply.github.com>
2021-01-13 23:05:02 +0100

    Initial commit

--- src/avak1/Main.java
+++ src/avak1/Main.java
@@ -7,6 +7,6 @@ public class Main {
         System.out.println("Hi !");
         System.out.println(" ");
         System.out.println("this is the first change");
-
+        System.out.println("after branch delete");
     }
 }
